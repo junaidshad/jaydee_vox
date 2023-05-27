0da9170d241493036e3433f04597e68274d5eecf
Junaid Shad <xadxunaid@gmail.com>
2023-05-27 14:21:36 +0500

    proj init

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="hero">
+      <div>Welcome to Voxel</div>
+      <span>Discover our wide range of products</span>
+    </div>
+
+    <div class="container main-page">
+      <CategoryFilter />
+      <div class="grid-view">
+        <Product />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+
+}
+</script>
